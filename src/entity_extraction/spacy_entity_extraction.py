f278813271ad2f05363fc520412bc8c80c024cbb
Jenit Jain <jenitjai@student.ubc.ca>
2023-05-25 23:29:54 -0700

    Spacy ner evaluation setup

---
+++ src/entity_extraction/spacy_entity_extraction.py
@@ -0,0 +1,15 @@
+# Author: Ty Andrews, Jenit Jain
+# Date: May 10, 2023
+
+import os
+import sys
+import json
+import pandas as pd
+import spacy
+
+# ensure that the parent directory is on the path for relative imports
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+def spacy_entity_extraction():
+    # Placeholder for spacy entity extraction functions
+    pass

7010a78f48072de90e9eeefd721734950f524e4b
Jenit Jain <jenitjai@student.ubc.ca>
2023-05-27 10:56:56 -0700

    Use fine-tuned spacy model for inference

--- src/entity_extraction/spacy_entity_extraction.py
+++ src/entity_extraction/spacy_entity_extraction.py
@@ -3,13 +3,49 @@
 
 import os
 import sys
-import json
-import pandas as pd
 import spacy
 
 # ensure that the parent directory is on the path for relative imports
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-def spacy_entity_extraction():
-    # Placeholder for spacy entity extraction functions
-    pass
+
+def spacy_extract_all(text: str,
+                            ner_model=None,
+                            model_path=os.path.join(
+                                os.pardir,
+                                "models",
+                                "transformer-v1")):
+    """
+    Extracts entities from text using a spacy model
+
+    Parameters
+    ----------
+    text : str
+        The text to extract entities from
+    ner_model : spacy model
+        The spacy model to use for entity extraction
+    model_path : str
+        The path to the spacy model to use for entity extraction
+    
+    Returns
+    -------
+    entities : list
+        A list of entities and their metadata
+    """
+
+    if ner_model == None:
+        spacy.require_cpu()
+        ner_model = spacy.load(model_path)
+
+    entities = []
+    doc = ner_model(text)
+
+    for ent in doc.ents:
+        entities.append({
+            "start": ent.start_char,
+            "end": ent.end_char,
+            "labels": [ent.label_],
+            "text": ent.text
+        })
+
+    return entities

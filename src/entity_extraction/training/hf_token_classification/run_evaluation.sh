7361e91ff97bf711314515f142c3205ba6956030
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-05-30 16:20:57 -0700

    feat: bash script to run evaluation on checkpoint

---
+++ src/entity_extraction/training/hf_token_classification/run_evaluation.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh 
+
+# if running with conda envs, comment out if not
+conda activate ffossils
+
+echo python.__version__ = $(python -c 'import sys; print(sys.version)')
+# ensure we're in the MetaExtractor root directory
+echo "Current working directory: $(pwd)"
+
+# set the location of the labelled data, ideally this is run from root of repo
+export DATA_DIR="$(pwd)/data/labelled/2023-05-29_label-export/hf_processed/"
+export MODEL_PATH="$(pwd)/models/ner/roberta-finetuned/checkpoint-750"
+export OUTPUT_DIR="$(pwd)/results/ner/test-results/"
+export MODEL_NAME="roberta-finetuned"
+
+# process the labelled files to prepare them for training
+python src/entity_extraction/training/hf_token_classification/hf_evaluate.py \
+    --data_path $DATA_DIR \
+    --model_path $MODEL_PATH \
+    --output_path $OUTPUT_DIR \
+    --model_name $MODEL_NAME \
+
+# use this for local cpu testing
+    # --max_samples 1

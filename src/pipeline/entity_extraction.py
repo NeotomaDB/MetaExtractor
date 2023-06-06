8ae5d7be7d4714cfbe13e639836c27f578903ea1
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-06-05 18:07:20 -0700

    feat: initial NER pipeline framework

---
+++ src/pipeline/entity_extraction.py
@@ -0,0 +1,120 @@
+# Author: Ty Andrews
+# Date: 2023-06-05
+"""
+Usage: entity_extraction.py --relevance_results_path=<relevance_results_path> --article_text_path <article_text_path> --output_path=<output_path>
+
+Options:
+--relevance_results_path=<relevance_results_path> The path to the article relevance results file.
+--article_text_path=<article_text_path> The path to the article text data file.
+--output_path=<output_path> The path to export the extracted entities to.
+"""
+
+import os
+import sys
+
+import pandas as pd
+from docopt import docopt
+
+def load_relevant_articles(relevance_results_path:str)-> pd.DataFrame:
+    """
+    Loads the relevant articles from the article relevance results file.
+
+    Parameters
+    ----------
+    relevance_results_path : str
+        The path to the article relevance results file.
+
+    Returns
+    -------
+    pd.DataFrame
+        The relevant articles.
+    """
+
+    return pd.DataFrame()
+
+def load_article_text_data(article_text_path:str)-> pd.DataFrame:
+    """
+    Loads the article text data from the article text data file.
+
+    Parameters
+    ----------
+    article_text_path : str
+        The path to the article text data file.
+
+    Returns
+    -------
+    pd.DataFrame
+        The article text data.
+    """
+
+    return pd.DataFrame()
+
+def preprocess_article_text_data(article_text_data:pd.DataFrame)-> pd.DataFrame:
+    """
+    Preprocesses the article text data.
+
+    Parameters
+    ----------
+    article_text_data : pd.DataFrame
+        The article text data.
+
+    Returns
+    -------
+    pd.DataFrame
+        The preprocessed article text data.
+    """
+
+    return pd.DataFrame()
+
+def extract_entities(article_text_data:pd.DataFrame)-> pd.DataFrame:
+    """
+    Extracts the entities from the article text data.
+
+    Parameters
+    ----------
+    article_text_data : pd.DataFrame
+        The article text data.
+
+    Returns
+    -------
+    pd.DataFrame
+        The extracted entities.
+    """
+
+    return pd.DataFrame()
+
+def post_process_extracted_entities(extracted_entities:pd.DataFrame)-> dict:
+    """
+    Post-processes the extracted entities into the format to be exported to json.
+
+    Parameters
+    ----------
+    extracted_entities : pd.DataFrame
+        The extracted entities.
+
+    Returns
+    -------
+    dict
+        The post-processed extracted entities.
+    """
+
+    return dict()
+
+def export_extracted_entities(extracted_entities:dict, output_path:str)-> None:
+    """
+    Exports the extracted entities to json.
+
+    Parameters
+    ----------
+    extracted_entities : dict
+        The extracted entities.
+    output_path : str
+        The path to export the extracted entities to.
+    """
+
+    return None
+
+def main():
+
+    opt = docopt(__doc__)
+    

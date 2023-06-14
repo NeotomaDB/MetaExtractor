ddd0f4ea31ccfada2d13e208f6fe5062dfd74eee
Shaun Hutchinson <shaunhutch@shaw.ca>
2023-06-14 16:19:30 -0700

    Cleaned up code and added docstring

--- src/data_review_tool/pages/navbar.py
+++ src/data_review_tool/pages/navbar.py
@@ -1,9 +1,6 @@
-import dash
-from dash import dcc, html
+
+from dash import html
 import dash_bootstrap_components as dbc
-import pandas as pd
-import json
-import dash_mantine_components as dmc
 from pages.config import *
 
 def create_navbar():
@@ -49,31 +46,17 @@ def create_navbar():
     )
     return navbar
 
-# The following two functions were taken from https://stackoverflow.com/questions/54776916/inverse-of-pandas-json-normalize
-def _get_nested_fields(df: pd.DataFrame):
-    """Return a list of nested fields, sorted by the deepest level of nesting first."""
-    nested_fields = [*{field.rsplit(".", 1)[0] for field in df.columns if "." in field}]
-    nested_fields.sort(key=lambda record: len(record.split(".")), reverse=True)
-    return nested_fields
-
-def df_denormalize(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Convert a normalised DataFrame into a nested structure.
-
-    Fields separated by '.' are considered part of a nested structure.
-    """
-    nested_fields = _get_nested_fields(df)
-    for field in nested_fields:
-        list_of_children = [column for column in df.columns if field in column]
-        rename = {
-            field_name: field_name.rsplit(".", 1)[1] for field_name in list_of_children
-        }
-        renamed_fields = df[list_of_children].rename(columns=rename)
-        df[field] = json.loads(renamed_fields.to_json(orient="records"))
-        df.drop(list_of_children, axis=1, inplace=True)
-    return df
-
 def find_start_end_char(text, entity):
+    """Find the start and end character of an entity in a text.
+
+    Args:
+        text (str): Text to search for entity.
+        entity (str): Entity to search for in text.
+
+    Returns:
+        start (int): Start character of entity in text.
+        end (int): End character of entity in text.
+    """
     start = text.find(entity)
     if start == -1:
         end = -1
